e4300eb0e694af743bd683e88a7705f4adc37860
katagaki <68231035+katagaki@users.noreply.github.com>
2024-02-10 16:32:32 +0900

    Update project with komponents

--- Randomly/Views/More/MoreView.swift
+++ Randomly/Views/More/MoreView.swift
@@ -5,6 +5,7 @@
 //  Created by シンジャスティン on 2023/08/23.
 //
 
+import Komponents
 import SwiftUI
 
 struct MoreView: View {
@@ -13,53 +14,7 @@ struct MoreView: View {
 
     var body: some View {
         NavigationStack(path: $navigationManager.moreTabPath) {
-            List {
-                Section {
-                    Link(destination: URL(string: "https://x.com/katagaki_")!) {
-                        HStack {
-                            ListRow(image: "ListIcon.Twitter",
-                                    title: "More.Help.Twitter",
-                                    subtitle: "More.Help.Twitter.Subtitle",
-                                    includeSpacer: true)
-                            Image(systemName: "safari")
-                                .opacity(0.5)
-                        }
-                        .foregroundColor(.primary)
-                    }
-                    Link(destination: URL(string: "mailto:ktgk.public@icloud.com")!) {
-                        HStack {
-                            ListRow(image: "ListIcon.Email",
-                                    title: "More.Help.Email",
-                                    subtitle: "More.Help.Email.Subtitle",
-                                    includeSpacer: true)
-                            Image(systemName: "arrow.up.forward.app")
-                                .opacity(0.5)
-                        }
-                        .foregroundColor(.primary)
-                    }
-                    Link(destination: URL(string: "https://github.com/katagaki/Random")!) {
-                        HStack {
-                            ListRow(image: "ListIcon.GitHub",
-                                    title: "More.Help.GitHub",
-                                    subtitle: "More.Help.GitHub.Subtitle",
-                                    includeSpacer: true)
-                            Image(systemName: "safari")
-                                .opacity(0.5)
-                        }
-                        .foregroundColor(.primary)
-                    }
-                } header: {
-                    ListSectionHeader(text: "More.Help")
-                        .font(.body)
-                }
-                Section {
-                    NavigationLink(value: ViewPath.moreAttributions) {
-                        ListRow(image: "ListIcon.Attributions",
-                                title: "More.Attribution")
-                    }
-                }
-            }
-            .listStyle(.insetGrouped)
+            MoreList(repoName: "katagaki/Random", viewPath: ViewPath.moreAttributions) { }
             .navigationDestination(for: ViewPath.self, destination: { viewPath in
                 switch viewPath {
                 case .moreAttributions: LicensesView()
